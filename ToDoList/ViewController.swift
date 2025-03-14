f0310951978b6e71da90263ad24088785367e4e9
edy alves <30.edyalves@gmail.com>
2025-03-14 10:58:17 -0300

    Implemente TableView.

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -10,16 +10,27 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var topTitleView: UIView!
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         topTitleView.clipsToBounds = true
-        topTitleView.layer.cornerRadius = 15
+        topTitleView.layer.cornerRadius = 25
         topTitleView.layer.maskedCorners = [
             .layerMinXMaxYCorner, .layerMaxXMaxYCorner
         ]
+        tableView.dataSource = self
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
     
 }
