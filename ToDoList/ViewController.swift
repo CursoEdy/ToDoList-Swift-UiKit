b4038179977c6a77c163fa5907ffb1780ac623f0
edy alves <30.edyalves@gmail.com>
2025-03-17 10:05:57 -0300

    Create addButton, set config bottom position.

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -11,6 +11,14 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var topTitleView: UIView!
     @IBOutlet weak var tableView: UITableView!
+    lazy var addButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .link
+        button.tintColor = .white
+        button.setImage(UIImage(systemName: "plus"), for: .normal)
+        button.imageView?.layer.transform = CATransform3DMakeScale(1.4 , 1.4, 1.4)
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +30,20 @@ class ViewController: UIViewController {
         tableView.dataSource = self
         tableView.estimatedRowHeight = 80
         tableView.rowHeight = UITableView.automaticDimension
+        
+        view.addSubview(addButton)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let safeAreaBottom = view.safeAreaInsets.bottom
+        let width: CGFloat = 60
+        let height: CGFloat = 60
+        let xPos = view.frame.width / 2 - width / 2
+        let yPos = view.frame.height - height - safeAreaBottom
+        addButton.frame = CGRect(x: xPos, y: yPos, width: width, height: height)
+        addButton.layer.cornerRadius = width / 2
+        
     }
 }
 
