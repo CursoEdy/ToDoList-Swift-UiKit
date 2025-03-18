84c5add3914961a6ae77290a4069a6b28693fd1a
edy alves <30.edyalves@gmail.com>
2025-03-18 14:30:55 -0300

    NewTaskModelView prt2.

---
+++ ToDoList/CategoryModel.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryModel.swift
+//  ToDoList
+//
+//  Created by ednardo alves on 18/03/25.
+//
+
+import Foundation
+
+enum Category: String, CaseIterable {
+    case work, study, exercise
+}
--- ToDoList/NewTaskModelView.swift
+++ ToDoList/NewTaskModelView.swift
@@ -22,6 +22,9 @@ class NewTaskModelView: UIView {
         descriptionTextView.text = "Digite a sua tarefa aqui..."
         descriptionTextView.textColor = UIColor.lightGray
         descriptionTextView.delegate = self
+        categoryPickerView.dataSource = self
+        categoryPickerView.delegate = self
+        categoryPickerView.selectRow(1, inComponent: 0, animated: true)
     }
     
     @IBAction func submit(_ sender: Any) {
@@ -47,4 +50,46 @@ extension NewTaskModelView: UITextViewDelegate {
         }
     }
     
+}
+
+extension NewTaskModelView: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return Category.allCases.count
+    }
+}
+
+extension NewTaskModelView: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let category = Category.allCases[row]
+        
+        //Exemplo de retorno usando String
+        return category.rawValue
+        
+        // Exemplo de retorno usando CaseIterable
+//        switch category {
+//        case .exercise:
+//            return "Exercício"
+//        case .study:
+//            return "Estudo"
+//        case .work:
+//            return "Trabalho"
+//        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        var pickerLabel: UILabel? = view as? UILabel
+        if pickerLabel == nil {
+            pickerLabel = UILabel()
+            pickerLabel?.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+            pickerLabel?.textAlignment = .center
+        }
+        let category = Category.allCases[row]
+        
+        pickerLabel?.text = category.rawValue
+        return pickerLabel!
+    }
 }
